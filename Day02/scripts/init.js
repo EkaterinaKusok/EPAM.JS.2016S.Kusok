59c655eddcdbf0bc1d266c6a21eea086005b831b
EkaterinaKusok <ekaterina.kusok@gmail.com>
2016-08-12 11:46:31 +0300

    update task2 (without if)

--- Day02/scripts/init.js
+++ Day02/scripts/init.js
@@ -6,30 +6,12 @@
 var array = [];
 for (var i = 0; i < 5; i++) {	
 	var type = random(1, 3);
-	if (type == 1) {
-		array[i] = {
-			count : random (1, 10),
-			GetCount1 : function() {
-				return this.count;
-			}
-		};
+	array[i] = {
+		count: random(1, 10)
+	};	
+	array[i]["GetCount" + type] = function() {
+		return this.count;
 	}
-	else if (type == 2) {
-		array[i] = {
-			count : random(1, 10),
-			GetCount2 : function() {
-				return this.count;
-			}
-		};
-	}
-	else {
-		array[i] = {
-			count : random(1, 10),
-			GetCount3 : function() {
-				return this.count;
-			}
-		};
-	}	
 	// Вывод в консоль информации о добавленном объекте в формате “type={0}, count={1}”.
 	console.log( "type={"+ type +"}, count={"+ array[i].count +"}");
 }
--- Day02/scripts/log.js
+++ Day02/scripts/log.js
@@ -2,18 +2,23 @@
 и вывести в консоль браузера три строки в формате “count{0}={1}”, где {0} – тип объекта, 
 {1} – сумма значений свойства “count” для данного типа. 
 */
-var count = [];
-count[1] = 0;
-count[2] = 0;
-count[3] = 0;
+var sum = [];
+sum[1] = 0;
+sum[2] = 0;
+sum[3] = 0;
 
 // Считаем общую сумму значений свойства “count” для каждого из типов
 for (var i = 0; i < array.length; i++) {
-	count[1] += (array[i].GetCount1 ?  array[i].GetCount1() : 0);
-	count[2] += (array[i].GetCount2 ?  array[i].GetCount2() : 0);
-	count[3] += (array[i].GetCount3 ?  array[i].GetCount3() : 0);
+	// sum[1] += (array[i].GetCount1 ? array[i].GetCount1() : 0);
+	sum[1] += (array[i].GetCount1 || zeroFunction).call(array[i]);	
+	sum[2] += (array[i].GetCount2 || zeroFunction).call(array[i]);	
+	sum[3] += (array[i].GetCount3 || zeroFunction).call(array[i]);
 }
 
 for (var i = 1; i < 4; i++) {
-	console.log("count"+ i + "=" + count[i]);
+	console.log("count"+ i + "=" + sum[i]);
+}
+
+function zeroFunction(){
+	return 0;
 }
